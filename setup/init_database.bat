1a1b7cd77c0da57f2a39a77814d668933397ea9b
MartinStevko <mstevko10@gmail.com>
2018-03-28 15:19:06 +0200

    Setup init file

--- setup/init_database.bat
+++ setup/init_database.bat
@@ -1,1 +1,28 @@
+@echo off
+title Naboj
 
+REM Project directory
+cd %~dp0
+cd ..
+pause
+
+REM Migrations
+manage.py makemigrations obdlznik
+manage.py makemigrations trojuholnik
+
+manage.py migrate
+
+echo Migrations successfully created!
+pause
+
+REM Superuser
+manage.py createsuperuser
+pause
+
+REM Database creation
+manage.py shell < .\base_data.txt
+echo Database successfully created!
+pause
+
+REM Server initiation
+manage.py runserver 0.0.0.0:80
